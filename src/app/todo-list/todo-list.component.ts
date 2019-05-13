7009fd94592fb582c1a486c4ef6a4300bc5681b6
reynaldo <reynaldozig@hotmail.com>
2019-05-13 16:33:52 -0400

    Finish CRUD Functions

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -7,16 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-
+  items: any[];
   constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
+    // this.db.collection('items').snapshotChanges().subscribe(data => {
+    //   this.items = data;
+    // });
+    this.db.collection('items').snapshotChanges().subscribe(data => {
+      this.items = [];
+      data.forEach(a => {
+        const item: any = a.payload.doc.data();
+        item.id = a.payload.doc.id;
+        this.items.push(item);
+      });
+    });
   }
 
   add() {
-    this.db.collection('items').add({
+    this.db.collection(`items`).add({
       timestamp: new Date()
     });
+  }
+
+  update(item) {
+    this.db.doc(`items/${item.id}`).update({
+      timestamp: new Date()
+    });
+  }
+
+  delete(item) {
+    this.db.doc(`items/${item.id}`).delete();
   }
 
 }
